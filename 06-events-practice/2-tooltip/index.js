c9b233afdb41ab064b885234d1e796b2e3886548
Alex Timoshenko <ultimatevs@mail.ru>
2022-02-06 23:34:02 +0300

    fixed sortable-table-v2, added tooltip and column-chart with async

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,65 @@
 class Tooltip {
-  initialize () {
+  element;
+  static init;
 
+  constructor() {
+    if(Tooltip.init) {
+      return Tooltip.init;
+    }
+    Tooltip.init = this;
+  }
+
+  onPointerOver = (event) => {
+    const element = event.target.closest('[data-tooltip]');
+    if(element) {
+      this.render(element.dataset.tooltip);
+      document.addEventListener('pointermove', this.onPointerMove);
+    }
+  }
+
+  onPointerMove = (event) => {
+    const margin = 10;
+    const left = event.clientX + margin;
+    const top = event.clientY + margin;
+
+    this.element.style.left = `${left}px`;
+    this.element.style.top = `${top}px`;
+  }
+
+  onPointerOut = () => {
+    this.remove();
+    document.removeEventListener('pointermove', this.onPointerMove);
+  }
+
+  initialize () {
+    this.initEventListeners();
+  }
+
+  initEventListeners() {
+    document.addEventListener('pointerover', this.onPointerOver);
+    document.addEventListener('pointerout', this.onPointerOut);
+  }
+
+  render(text) {
+    this.element = document.createElement('div');
+    this.element.className = 'tooltip';
+    this.element.innerHTML = text;
+
+    document.body.append(this.element);
+  }
+
+  remove() {
+    if(this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    document.removeEventListener('pointerover', this.onPointerOver);
+    document.removeEventListener('pointermove', this.onPointerMove);
+    document.removeEventListener('pointerout', this.onPointerOut);
+    this.element = null;
   }
 }
 
